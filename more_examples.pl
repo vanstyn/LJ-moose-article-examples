4fd0244aa5be1d72a7cee27728810ce6eaa10b5a
Henry Van Styn <vanstyn@intellitree.com>
2014-10-21 18:41:43 -0400

    tweaked avg_rifle jam_probability to be different

--- more_examples.pl
+++ more_examples.pl
@@ -14,7 +14,7 @@ print "Now there are " . $rifle->rounds . " rounds in the rifle\n";
 
 print "\n\n===\n\n";
 
-my $avg_rifle = MyApp::NiceRifle->new( jam_probability => 0.01, rounds => 13 );
+my $avg_rifle = MyApp::NiceRifle->new( jam_probability => 0.05, rounds => 13 );
 print "There are " . $avg_rifle->rounds . " rounds in the average rifle\n";
 $avg_rifle->fire_all;
 print "Now there are " . $avg_rifle->rounds . " rounds in the average rifle\n";
